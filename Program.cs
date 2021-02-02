f3df599bf7e1a69889aaa5351959f05c84a6f9bd
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-02 09:16:35 +0200

    third way solving , using mehtod Array.Sort() to sort ascending the numbers

--- Program.cs
+++ Program.cs
@@ -12,21 +12,7 @@ namespace Arrange_ArrayAscending
                 .Select(int.Parse)  // inittialize an  array; // 5 2 4 3
                 .ToArray();
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                // int currElement = numbers[i]; // we take the first number of the numbers[]
-
-                for (int j = i + 1; j < numbers.Length; j++)  // We rotate from second number to end number
-                {
-                    if (numbers[i] > numbers[j])  // if this is true
-                    {
-                        int tempNumb = numbers[i];
-                        numbers[i] = numbers[j]; // we change their places; ---second number become first
-                        numbers[j] = tempNumb; // first number becomes second 
-                                               // we have a new first number--minNumber so we reinitialize the currNumber
-                    }
-                }
-            }
+            Array.Sort(numbers);
 
             Console.WriteLine();
             foreach (var item in numbers)
